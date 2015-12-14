98f5954bc68c02086d49afae66e42ba2c6b4d5ad
code56 <evanthiak432@gmail.com>
2015-12-14 09:50:48 +0000

    Update testing.py

--- testing.py
+++ testing.py
@@ -27,9 +27,27 @@ Hello1 = """ <article-categories>
        </article-categories> 
 """
 
-import os
+import glob, os
 import xml.dom.minidom
 dom = xml.dom.minidom.parseString(hello)
+
+
+#path_elife_articles_folder = os.path.dirname(os.path.abspath("elife_articles_master"))    # path to the folder containing the MGB output files. The folder is in the current directory 
+
+for root,dirs,files in os.walk("elife_articles_master"):
+    for file in files:
+      if file.endswith('.xml'):
+        print file
+
+
+# or because the elife_articles_master is in the working directory, I can simply put 
+# from elife_articles_master 
+
+# for i in range(1, (len(onlyfiles)+1)):        #for all the .gbk files in the antiSMASH output files containing folder, take them one at a time
+#    #create the path for some of the input arguments of the antiSMASH_processing.py
+#     inputFilename = inputstring + '%0.3d'%i + '.gbk'                               #NC_003888.3.clusterxxx.gbk
+#     outputFilename = outputstring + '%0.3d'%i + '_cds.fasta'                       #NC_003888.3.clusterxxx_cds.fasta
+#     outputFilename2 = outputstring1 + '%0.3d'%i + 'codonFreqs.txt'                 #NC_003888.3.clusterxxxcodonFreqs.txt
 
 
 # parser = argparse.ArgumentParser()
@@ -41,18 +59,7 @@ dom = xml.dom.minidom.parseString(hello)
 # outputstring1 = path_anti_folder + "/cds_files/" + fileWithoutExt[0]+ ".3.cluster "   #save the synonymous codon usage of all sequences in the cluster in a separate "cds_file" folder  
 #                         # retain the name of the input antiSMASH cluster file 
 
-# for i in range(1, (len(onlyfiles)+1)):        #for all the .gbk files in the antiSMASH output files containing folder, take them one at a time
-#    #create the path for some of the input arguments of the antiSMASH_processing.py
-#     inputFilename = inputstring + '%0.3d'%i + '.gbk'                               #NC_003888.3.clusterxxx.gbk
-#     outputFilename = outputstring + '%0.3d'%i + '_cds.fasta'                       #NC_003888.3.clusterxxx_cds.fasta
-#     outputFilename2 = outputstring1 + '%0.3d'%i + 'codonFreqs.txt'                 #NC_003888.3.clusterxxxcodonFreqs.txt
 
-path_elife_articles_folder = os.path.dirname(os.path.abspath(elife_articles_master))    # find the path to the folder containing the MGB output files. The folder is in the current directory 
-
-# or because the elife_articles_master is in the working directory, I can simply put 
-# from elife_articles_master 
-
-print "the path to elife_articles_master is", path_elife_articles_folder
 
 
 # mgb_folder_path = path_mgb_folder + "/" + mgb_folder + "/"  # go to the path of the MGB output file folder, i.e. access the folder.
