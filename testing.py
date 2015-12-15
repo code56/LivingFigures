a1ec35b34b55f6657a171e3c0cc32d455b87df9f
code56 <evanthiak432@gmail.com>
2015-12-15 21:23:55 +0000

    Update testing.py

--- testing.py
+++ testing.py
@@ -63,47 +63,7 @@ def main():
     f.close
 
 
-  # folder = 'elife_articles_master'
 
-
-  # for filename in os.listdir(folder):
-  #     infilename = os.path.join(folder, filename)
-  #     print "infilename", infilename
-  #     if not os.path.isfile(infilename): continue
-
-  #     base, extension = os.path.splitext(filename)
-  #     infile = open(infilename, 'r')
-  #     print infile
-  #     outfile = open(os.path.join(folder, '{}_edit.{}'.format(base, extension)), 'w')
-
-
-  # or because the elife_articles_master is in the working directory, I can simply put 
-  # from elife_articles_master 
-
-  # for i in range(1, (len(onlyfiles)+1)):        #for all the .gbk files in the antiSMASH output files containing folder, take them one at a time
-  #    #create the path for some of the input arguments of the antiSMASH_processing.py
-  #     inputFilename = inputstring + '%0.3d'%i + '.gbk'                               #NC_003888.3.clusterxxx.gbk
-  #     outputFilename = outputstring + '%0.3d'%i + '_cds.fasta'                       #NC_003888.3.clusterxxx_cds.fasta
-  #     outputFilename2 = outputstring1 + '%0.3d'%i + 'codonFreqs.txt'                 #NC_003888.3.clusterxxxcodonFreqs.txt
-
-
-  # parser = argparse.ArgumentParser()
-  # parser.add_argument('inputfile', nargs='?', default=argparse.SUPPRESS)             
-
-  # parser.add_argument('-inputfile',  dest='inputfile', default='NO FILE SUPPLIED')
-
-  # outputstring = path_anti_folder + "/" + "fasta_files" + "/" + fileWithoutExt[0]+ ".3.cluster "  #the path to create the files created after the antiSMASH output handling- creates in fasta_files                           #folder and the files are named automatically retaining their parent cluster name.
-  # outputstring1 = path_anti_folder + "/cds_files/" + fileWithoutExt[0]+ ".3.cluster "   #save the synonymous codon usage of all sequences in the cluster in a separate "cds_file" folder  
-  #                         # retain the name of the input antiSMASH cluster file 
-
-
-
-
-  # mgb_folder_path = path_mgb_folder + "/" + mgb_folder + "/"  # go to the path of the MGB output file folder, i.e. access the folder.
-
-  # onlyfiles1 = [f for f in listdir(mgb_folder_path) if isfile(join(mgb_folder_path, f))] #the list that holds the number of MGB output files in the folder
-
-  # inputstring_mgb = path_mgb_folder + "/" + mgb_folder + "/" + mgb_folder
 
 def getText(nodelist):
   rc = ""
